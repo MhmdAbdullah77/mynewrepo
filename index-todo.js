8f878c40cb67ab3510e5cd9f1cac8074795ffaa5
Abdullah <abdulbrothers10@gmail.com>
2024-12-09 01:52:12 +0500

    todo list

---
+++ index-todo.js
@@ -0,0 +1,35 @@
+const input = document.querySelector('#input-box');
+const list = document.querySelector('#list-container');
+const button = document.querySelector('button');
+button.addEventListener('click',function(){
+    if(input.value === ''){
+        alert('you must erite something')
+    }else{
+        let li = document.createElement('li');
+        li.innerHTML = input.value
+        list.append(li)
+        let span = document.createElement('span');
+        span.innerHTML = '\u00d7'
+        li.appendChild(span)
+        }
+    input.value = ''
+    saveData();
+})
+list.addEventListener('click',function(e){
+if(e.target.tagName === 'LI' ){
+e.target.classList.toggle('checked')
+saveData();
+}
+else if(e.target.tagName === 'SPAN' ){
+e.target.parentElement.remove()
+saveData();
+}
+})
+function saveData(){
+    localStorage.setItem('data',list.innerHTML)
+}
+function showTask(){
+list.innerHTML = localStorage.getItem('data')
+}
+showTask();
+
