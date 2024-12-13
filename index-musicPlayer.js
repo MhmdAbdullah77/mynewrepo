20d6e3abb91f761bd023eb600544326dd6ff597e
Abdullah <abdulbrothers10@gmail.com>
2024-12-13 09:31:43 +0500

    music player

---
+++ index-musicPlayer.js
@@ -0,0 +1,48 @@
+let progress = document.querySelector('#progress');
+let song = document.querySelector('#song');
+let controlIcon = document.querySelector('#ctrlIcon');
+
+
+controlIcon.addEventListener('click',function(){
+    playPause();
+})
+ 
+
+if(song.play()){
+    setInterval(()=>{
+        progress.value = song.currentTime;
+    },500)
+}
+progress.onchange = function(){
+    song.play();
+    song.currentTime = progress.value
+    controlIcon.classList.add("fa-pause")
+    controlIcon.classList.remove("fa-play")
+}
+
+song.onloadedmetadata() = function(){
+    progress.max = song.duration;
+    progress.value = song.currentTime;
+
+}
+
+function playPause(){
+    if(controlIcon.classList.contains("fa-pause")){
+        song.pause();
+        controlIcon.classList.remove("fa-pause")
+        controlIcon.classList.add("fa-play")
+    }else {
+        song.play()
+        controlIcon.classList.add("fa-pause")
+        controlIcon.classList.remove("fa-play")
+    }
+}
+
+
+
+progress.onchange = function(){
+    song.play();
+    song.currentTime = progress.value
+    controlIcon.classList.remove("fa-pause")
+    controlIcon.classList.add("fa-play")
+}
